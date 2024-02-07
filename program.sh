9f56d3bf81c7315f34e9b0bf72b379cf316e0423
Jakub <jakubstefaniak99@gmail.com>
2024-02-07 20:31:05 +0100

    uruchamianie

--- program.sh
+++ program.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+# Instalacja
+inst="pliki/INSTALL"
 
-# ścieżka do menu
+# Ścieżka do programu
 pwd="pliki/menu.py"
 
-# uruchomienie zmiennej przy użyciu pythona
-#cd "/pliki/Programs"
-python3 $pwd
+# Sprawdzenie dystrybucji i informowanie użytkownika
+if ! command -v apt-get &> /dev/null; then
+    echo "Wykryto, że apt-get nie jest dostępny w Twoim systemie."
+    echo "Upewnij się, że masz zainstalowany pipx przed kontynuacją."
+    read -p "Czy chcesz kontynuować? (y/n) " choice
+    if [[ $choice != "y" ]]; then
+        echo "Przerwano instalację."
+        exit 1
+    fi
+fi
+
+# Uruchomienie instalacji z pliku
+source "$inst" || { echo "Nie można wykonać pliku $inst"; exit 1; }
+
+# Uruchomienie skryptu Python po instalacji
+python3 "$pwd"
